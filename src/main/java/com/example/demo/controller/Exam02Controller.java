d0235231805809756613e7755943c9a9c5d6719d
gonzuiswimmer <gonzuiswimmer@gmail.com>
2023-02-02 14:50:37 +0900

    exam02の実装のため、exam02-result.html、result2.html、コントローラーを実装

--- src/main/java/com/example/demo/controller/Exam02Controller.java
+++ src/main/java/com/example/demo/controller/Exam02Controller.java
@@ -1,26 +1,40 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.form.Exam02Form;
+
+import jakarta.servlet.http.HttpSession;
+
 @Controller
-@RequestMapping("ex2")
+@RequestMapping("ex02")
 public class Exam02Controller {
+
+	@Autowired
+	HttpSession session;
+
 	@GetMapping("")
-	public String index() {
+	public String index(Model model, Exam02Form form) {
 		return "exam02";
 	}
-	
+
 	@PostMapping("/calc")
-	public String calc() {
+	public String calc(Model model, Exam02Form form, HttpSession session) {
+		int sum = form.getNum1() + form.getNum2();
+		session.setAttribute("num1", form.getNum1());
+		session.setAttribute("num2", form.getNum2());
+		session.setAttribute("sum", sum);
 		return "exam02-result";
 	}
-	
+
 	@GetMapping("/toresult")
-	public String toresult() {
+	public String toresult(Model model) {
 		return "exam02-result2";
 	}
-	
+
 }
